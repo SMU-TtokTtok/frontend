e461336dacdbaac597e3ac732132c3a99236b696
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-06-18 00:38:41 +0900

    design: minheight 80vh 추가

--- src/app/layout.css.ts
+++ src/app/layout.css.ts
@@ -2,5 +2,7 @@ import { vars } from '@/common/styles/theme.css';
 import { style } from '@vanilla-extract/css';
 
 export const withFooterContent = style({
-  backgroundColor: vars.colors.surface.default,
+  marginTop: '4.222rem',
+  backgroundColor: vars.colors.surface.variant,
+  minHeight: '80vh',
 });
