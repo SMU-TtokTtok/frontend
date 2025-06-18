a805e983a3b1d08f85c2f1161f3dbe3207fac3b9
chahyunwoo00 <ckgusdn8@naver.com>
2025-06-18 18:18:03 +0900

    feat: (without-footer) layout 추가

--- src/app/layout.css.ts
+++ src/app/layout.css.ts
@@ -2,5 +2,13 @@ import { vars } from '@/common/styles/theme.css';
 import { style } from '@vanilla-extract/css';
 
 export const withFooterContent = style({
+  marginTop: '4.222rem',
   backgroundColor: vars.colors.surface.default,
+  minHeight: '80vh',
+});
+
+export const withoutFooterContent = style({
+  marginTop: '4.222rem',
+  backgroundColor: vars.colors.surface.default,
+  minHeight: '90vh',
 });
