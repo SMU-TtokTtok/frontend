9d44617eb9f3512bd75eb94ca66d996dc0fb6846
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-06-23 22:18:34 +0900

    feat: 동아리 리스트 구현

---
+++ src/common/components/clubItem/recruitStatus.tsx
@@ -0,0 +1,11 @@
+import Tag from '@/common/ui/tag';
+import * as S from './clubItem.css';
+function RecruitStatus({ isRecruiting }: { isRecruiting: boolean }) {
+  return (
+    <Tag className={S.tagStyle} variant={isRecruiting ? 'secondary' : 'tertiary'}>
+      {isRecruiting ? '모집중' : '모집마감'}
+    </Tag>
+  );
+}
+
+export default RecruitStatus;
--- src/components/home/clubList/index.tsx
+++ src/components/home/clubList/index.tsx
@@ -1,5 +1,23 @@
-function ClubList() {
-  return <div>index</div>;
+'use client';
+import { useClubSInfinite } from '@/hooks/useClubsInfinite';
+import * as S from './clubList.css';
+import ClubItem from '@/common/components/clubItem';
+import { SearchQueryReturn } from '@/hooks/useSearchQuery';
+interface ClubListProps {
+  selectedOptions: SearchQueryReturn;
+}
+
+function ClubList({ selectedOptions }: ClubListProps) {
+  const { data } = useClubSInfinite({ selectedOptions });
+  // 필터링 적용후 무한스크롤 구현할게요! by 형준
+  return (
+    <div className={S.container}>
+      <ul className={S.innerWrapper}>
+        {data &&
+          data.map((club) => <ClubItem key={club.id} className={S.cardStyle} clubData={club} />)}
+      </ul>
+    </div>
+  );
 }
 
 export default ClubList;
