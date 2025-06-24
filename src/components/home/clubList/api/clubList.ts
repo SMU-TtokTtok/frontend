bd7b244315d22aada4c7a0d10a48e0a2e674a5fe
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-06-24 14:33:04 +0900

    fix: 네트워크 요청 문제 해결

--- src/components/home/clubList/api/clubList.ts
+++ src/components/home/clubList/api/clubList.ts
@@ -2,7 +2,7 @@ import { mainClient } from '@/common/apis/ttockTtockClient';
 import { ClubItemInfo } from '@/common/model/club';
 
 export const getClubList = async (page: number) => {
-  const data = await mainClient.get<ClubItemInfo[]>(`/posts/2`);
+  const data = await mainClient.get<ClubItemInfo[]>(`/clubs`);
 
   return data;
 };
