4950467c3cd2f8d89dc54ec5d65c1dfd14e7b7e7
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-06-28 02:37:29 +0900

    feat: 지원자 관련 키 추가

--- src/hooks/queries/key.ts
+++ src/hooks/queries/key.ts
@@ -2,3 +2,7 @@ export const clubDataKey = {
   popularClubList: ['popularClubList'],
   allClubList: ['allClubList'],
 } as const;
+
+export const applicantDataKey = {
+  applicantList: ['applicantList'],
+} as const;
