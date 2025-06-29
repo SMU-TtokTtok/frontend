0781665ded66529c1b9b7344d2345852f58f3583
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-06-30 00:48:27 +0900

    feat: 합불합 모킹 함수 추가

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,4 +1,12 @@
-import { Applicants, PatchApplicantStatus } from './applicants';
+import { Applicants, FailList, PassList, PatchApplicantStatus } from './applicants';
 import { Clubs, patchFavorite, popularClubs } from './clubList';
 
-export const handlers = [popularClubs, Clubs, patchFavorite, Applicants, PatchApplicantStatus];
+export const handlers = [
+  popularClubs,
+  Clubs,
+  patchFavorite,
+  Applicants,
+  PatchApplicantStatus,
+  PassList,
+  FailList,
+];
