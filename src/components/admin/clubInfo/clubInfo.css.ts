7480b486f8110e56b22cf5b2a39148a96014f1c8
chahyunwoo00 <ckgusdn8@naver.com>
2025-07-01 18:44:46 +0900

    feat: 관리자 동아리 상세정보 페이지

---
+++ src/components/admin/clubInfo/clubInfo.css.ts
@@ -0,0 +1,37 @@
+import { vars } from '@/common/styles/theme.css';
+import { style } from '@vanilla-extract/css';
+// import { recipe } from '@vanilla-extract/recipes';
+
+export const container = style({
+  paddingLeft: '21.222rem',
+  paddingRight: '27.778rem',
+});
+
+export const wrapper = style({
+  maxWidth: '1038px',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'start',
+  // alignItems: 'start',
+});
+
+export const title = style({
+  fontSize: vars.fonts.title3,
+  fontWeight: '700',
+  marginTop: '86px',
+  marginBottom: '20px',
+});
+
+export const flexRow = style({
+  gap: '24px',
+  display: 'flex',
+});
+
+export const card = style({
+  padding: '18px 24px',
+});
+export const clubName = style({
+  fontSize: vars.fonts.title3,
+  fontWeight: '600',
+});
