3dbb8ec13ce9b0310dfaef1f69472646ef4a3692
chahyunwoo00 <ckgusdn8@naver.com>
2025-07-01 18:41:32 +0900

    feat: handler  msw추가

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,4 +1,14 @@
 import { Applicants, PatchApplicantStatus } from './applicants';
 import { Clubs, patchFavorite, popularClubs } from './clubList';
+import { getClubInfo, patchClubInfo, patchRecruiting } from './clubInfo/clubInfo';
 
-export const handlers = [popularClubs, Clubs, patchFavorite, Applicants, PatchApplicantStatus];
+export const handlers = [
+  popularClubs,
+  Clubs,
+  patchFavorite,
+  Applicants,
+  PatchApplicantStatus,
+  getClubInfo,
+  patchClubInfo,
+  patchRecruiting,
+];
