8e2b1ce18b7de31f90b705bcf7755f3547244bdf
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-07-03 19:09:40 +0900

    feat: 지원 폼 인덱스 페이지

---
+++ src/components/admin/applicationForm/createform/step3/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Indicator from '../indicator';
+
+function FormQuestionStep() {
+  return (
+    <div>
+      <Indicator step={3} />
+    </div>
+  );
+}
+
+export default FormQuestionStep;
