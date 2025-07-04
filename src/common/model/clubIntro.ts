0fc81fcf0ba36e45b7ce810f09397c4a9eb85d76
chahyunwoo00 <ckgusdn8@naver.com>
2025-07-04 12:53:33 +0900

    feat: 동아리 소개 인터페이스

---
+++ src/common/model/clubIntro.ts
@@ -0,0 +1,15 @@
+export interface ClubIntro {
+  id: number;
+  name: string;
+  shortDescription: string;
+  type: string;
+  category: string;
+  detailField: string;
+  isRecruiting: boolean;
+  introduction: string;
+  recruitPeriod: string;
+  recruitTarget: string;
+  recruitNumber: string;
+  img: string;
+  peopleCount: number;
+}
