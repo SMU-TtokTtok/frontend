00b9dd917c3a42f2dc3a60f99eb3c315c1ac2ae6
chahyunwoo00 <ckgusdn8@naver.com>
2025-07-05 12:23:48 +0900

    feat: club 소개 컴포넌트

--- src/app/(with-footer)/club/[clubId]/page.tsx
+++ src/app/(with-footer)/club/[clubId]/page.tsx
@@ -3,6 +3,7 @@
 import * as S from '@/components/clubInfo/index.css';
 import BackButton from '@/components/clubInfo/BackButton';
 import ClubProfile from '@/components/clubInfo/ClubProfile';
+import ClubIntroduce from '@/components/clubInfo/ClubIntro';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { getClubInfo } from '@/components/clubInfo/api/getClubInfo';
@@ -30,6 +31,7 @@ export default function Page() {
       <div className={S.wrapper}>
         <BackButton />
         <ClubProfile clubIntro={clubIntro} />
+        <ClubIntroduce introduction={clubIntro.introduction} />
       </div>
     </div>
   );
---
+++ src/components/clubInfo/clubIntro.css.ts
@@ -0,0 +1,27 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from '@/common/styles/theme.css';
+
+export const container = style({
+  width: '100%',
+  marginBottom: '180px',
+});
+
+export const headerContainer = style({
+  display: 'flex',
+});
+
+export const headerItem = style({
+  flex: '1 0 0',
+  textAlign: 'center',
+  cursor: 'pointer',
+  padding: '20px 0 ',
+  borderRadius: '6px 6px 0px 0px',
+  fontWeight: 700,
+  fontSize: vars.fonts.body2,
+});
+
+export const contentContainer = style({
+  width: '100%',
+  padding: '26px 28px',
+  backgroundColor: 'white',
+});
