cc5189161b4e2c9e4cc4fedcdbe5d7c4c1de863d
chahyunwoo00 <ckgusdn8@naver.com>
2025-07-08 15:05:23 +0900

    file: 스키마 파일분리

--- src/app/(with-footer)/password/page.tsx
+++ src/app/(with-footer)/password/page.tsx
@@ -4,37 +4,17 @@ import * as S from '@/components/password/index.css';
 import Button from '@/common/ui/button';
 import Input from '@/common/ui/input';
 import { useForm } from 'react-hook-form';
-import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useState } from 'react';
-
-const schema = z
-  .object({
-    email: z
-      .string()
-      .email('올바른 이메일 형식이 아닙니다.')
-      .regex(/^[0-9]{2}\d{7}@sangmyung\.kr$/, '학교 이메일 형식이 아닙니다.'),
-    code: z.string().min(6, '인증코드는 6자리여야 합니다.').max(6, '인증코드는 6자리여야 합니다.'),
-    password: z
-      .string()
-      .min(8, '비밀번호는 최소 8자 이상이어야 합니다.')
-      .regex(/[!@#$%^&*(),.?":{}|<>]/, '특수문자를 포함해야 합니다.'),
-    passwordConfirm: z.string(),
-  })
-  .refine((data) => data.password === data.passwordConfirm, {
-    message: '비밀번호가 일치하지 않습니다.',
-    path: ['passwordConfirm'],
-  });
-
-type FormType = z.infer<typeof schema>;
+import { passwordSchema, PasswordFormType } from '@/components/password/schema';
 
 export default function Page() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormType>({
-    resolver: zodResolver(schema),
+  } = useForm<PasswordFormType>({
+    resolver: zodResolver(passwordSchema),
     mode: 'onBlur',
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
