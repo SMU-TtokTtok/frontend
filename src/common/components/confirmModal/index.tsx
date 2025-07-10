3272dfff91d916f44960f720bb81c2b63751a995
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-07-11 00:55:58 +0900

    feat: confirm modal 구현

---
+++ src/common/components/confirmModal/index.tsx
@@ -0,0 +1,28 @@
+import Button from '@/common/ui/button';
+import Modal from '@/common/ui/modal/modal';
+import { useOutsideClick } from '@/hooks/useOutsideClick';
+import { usePreventScroll } from '@/hooks/usepreventScroll';
+import { PropsWithChildren } from 'react';
+import * as S from './confirmModal.css';
+
+interface ConfirmModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+function ConfirmModal({ isOpen, onClose, children }: PropsWithChildren<ConfirmModalProps>) {
+  usePreventScroll(isOpen);
+  const ref = useOutsideClick(() => onClose());
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal.Content ref={ref} className={S.content}>
+        <Modal.Body className={S.body}>{children}</Modal.Body>
+        <Button variant="primary" className={S.Button}>
+          확인
+        </Button>
+      </Modal.Content>
+    </Modal>
+  );
+}
+
+export default ConfirmModal;
