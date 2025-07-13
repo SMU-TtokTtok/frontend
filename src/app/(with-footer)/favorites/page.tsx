5c045ca4ee4a0894ec5cc633c45b5efbd59145eb
chahyunwoo00 <ckgusdn8@naver.com>
2025-07-13 14:54:42 +0900

    refator: sort 정렬 QueryLink 컴포넌트 적용

--- src/app/(with-footer)/favorites/page.tsx
+++ src/app/(with-footer)/favorites/page.tsx
@@ -3,14 +3,14 @@
 import FavoritesClub from '@/components/favorites/FavoritesClub';
 import { useSearchQuery } from '@/hooks/useSearchQuery';
 import SortButtonGroup from '@/components/favorites/SortButtonGroup';
-import { ROUTES } from '@/common/constants/routes';
+// import { ROUTES } from '@/common/constants/routes';
 
 export default function Page() {
   const { filter } = useSearchQuery();
 
   return (
     <div>
-      <SortButtonGroup title="즐겨찾기" filter={filter} path={ROUTES.FAVORITES} />
+      <SortButtonGroup title="즐겨찾기" filter={filter} />
       <FavoritesClub selectedOptions={filter} />
     </div>
   );
