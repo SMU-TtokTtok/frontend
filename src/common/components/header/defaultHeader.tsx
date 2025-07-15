b3a0611b14c0564d474ebd66f262af85be61d376
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-07-15 16:10:55 +0900

    feat: 기본 헤더 구현

---
+++ src/common/components/header/defaultHeader.tsx
@@ -0,0 +1,35 @@
+'use client';
+import Link from 'next/link';
+import Header from '../../ui/header';
+import { ROUTES } from '@/common/constants/routes';
+import mainlogo from '@/assets/mainlogo_wh.svg';
+import Image from 'next/image';
+import { useScrollObserver } from '@/hooks/useScrollObserver';
+import * as S from './defaultHeader.css';
+import Button from '@/common/ui/button';
+
+function DefaultHeader() {
+  const isVisible = useScrollObserver();
+
+  return (
+    <Header isVisible={isVisible} className={S.InnerWrapper}>
+      <Link href={ROUTES.HOME}>
+        <Image src={mainlogo} className={S.Logo} alt="Main Logo" />
+      </Link>
+      <div>
+        <Link href={ROUTES.LOGIN}>
+          <Button className={S.ButtonStyle} variant="secondary">
+            로그인
+          </Button>
+        </Link>
+        <Link href={ROUTES.SIGNUP}>
+          <Button className={S.ButtonStyle} variant="secondary">
+            회원가입
+          </Button>
+        </Link>
+      </div>
+    </Header>
+  );
+}
+
+export default DefaultHeader;
