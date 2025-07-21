90b4c6fbdca971221f5a65e40e9da9c3525934c7
chahyunwoo00 <ckgusdn8@naver.com>
2025-07-21 15:15:19 +0900

    feat: 부원관리 서치바 적용

--- src/components/admin/clubMember/index.tsx
+++ src/components/admin/clubMember/index.tsx
@@ -1,10 +1,20 @@
+'use client';
+
+import { useState } from 'react';
 import * as S from './index.css';
+import SearchBarArea from './SearchBarArea';
 
 export default function ClubMemberPage() {
+  const [search, setSearch] = useState('');
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className={S.wrapper}>
       <div className={S.container}>
         <div className={S.title}>👥 부원 명단</div>
+        <SearchBarArea search={search} handleSearchChange={handleSearchChange} />
       </div>
     </div>
   );
