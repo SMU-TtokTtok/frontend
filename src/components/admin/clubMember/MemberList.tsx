5e098a4243619a0784fb728b6bb08e40988f7c21
chahyunwoo00 <ckgusdn8@naver.com>
2025-07-21 21:46:13 +0900

    feat: next 부원명단 api 구현

--- src/components/admin/clubMember/MemberList.tsx
+++ src/components/admin/clubMember/MemberList.tsx
@@ -1,3 +1,9 @@
+import { useClubMemberInfinite } from '@/hooks/useInfiniteCommon';
+
 export default function MemberList() {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, refetch } = useClubMemberInfinite({
+    enabled: true,
+  });
+  console.log(data);
   return <div>MemberList</div>;
 }
