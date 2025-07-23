71109c70adae1a7ac67352dd13601455c71c5da1
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-07-23 16:36:20 +0900

    refactor: 도메인 변경

--- src/common/apis/ttockTtockClient.tsx
+++ src/common/apis/ttockTtockClient.tsx
@@ -3,4 +3,4 @@ import ApiClient from './apiClient';
 export const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL ?? ''}`;
 
 export const mainClient = new ApiClient(BASE_URL, 'access_token');
-export const adminClient = new ApiClient(`${BASE_URL}/admin`, 'admin_access_token');
+export const adminClient = new ApiClient(`${BASE_URL}/api/admin`, 'admin_access_token');
