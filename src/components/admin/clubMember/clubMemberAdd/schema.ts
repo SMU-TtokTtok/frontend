5b5abf8d49af8651d0164249e6ef15a58f824940
chahyunwoo00 <ckgusdn8@naver.com>
2025-07-27 15:35:36 +0900

    feat: 부원추가 스키마 수정

--- src/components/admin/clubMember/clubMemberAdd/schema.ts
+++ src/components/admin/clubMember/clubMemberAdd/schema.ts
@@ -6,8 +6,14 @@ export const clubMemberAddSchema = z.object({
   major: z.string().min(1, '학과를 입력해주세요.'),
   email: z.string().email('올바른 이메일 형식을 입력해주세요.'),
   phone: z.string().min(1, '전화번호를 입력해주세요.'),
-  grade: z.string().min(1, '학년을 선택해주세요.'),
-  gender: z.string().min(1, '성별을 선택해주세요.'),
+  grade: z
+    .string()
+    .nullable()
+    .refine((val) => val !== null, '학년을 선택해주세요.'),
+  gender: z
+    .string()
+    .nullable()
+    .refine((val) => val !== null, '성별을 선택해주세요.'),
 });
 
 export type ClubMemberAddData = z.infer<typeof clubMemberAddSchema>;
