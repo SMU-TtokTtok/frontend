15f89c69090699fc6a696558bb25ad1963144994
chahyunwoo00 <ckgusdn8@naver.com>
2025-07-30 00:23:35 +0900

    feat: 영어->한국어 유틸함수 생성

--- src/common/util/convertGradeArrayToString.ts
+++ src/common/util/convertGradeArrayToString.ts
@@ -1,4 +1,13 @@
-export function convertGradeArrayToString(grades: number[]): string {
+export function convertGradeArrayToString(grades: string[]): string {
   if (!grades || grades.length === 0) return '';
-  return grades.join(', ') + '학년';
+
+  const gradeMap: Record<string, string> = {
+    FIRST_GRADE: '1',
+    SECOND_GRADE: '2',
+    THIRD_GRADE: '3',
+    FOURTH_GRADE: '4',
+  };
+
+  const convertedGrades = grades.map((grade) => gradeMap[grade]).filter(Boolean);
+  return convertedGrades.join(', ') + '학년';
 }
