bee49976807a15f2911606a048a1e20e4098a474
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-07-30 16:38:12 +0900

    refactor: 엔드포인트 상수화 추가 및 적용

--- src/components/admin/login/api/login.ts
+++ src/components/admin/login/api/login.ts
@@ -1,13 +1,17 @@
 import { adminClient } from '@/common/apis/ttockTtockClient';
+import { API } from '@/common/constants/endpoints';
 
 export const postLogin = async ({ login, password }: { login: string; password: string }) => {
-  const data = await adminClient.post(`/auth/login`, { username: login, password: password });
+  const data = await adminClient.post(`${API.ADMIN.LOGIN}`, {
+    username: login,
+    password: password,
+  });
 
   return data;
 };
 
 export const postAdminRefresh = async () => {
-  const data = await adminClient.post(`/auth/re-issue`, {});
+  const data = await adminClient.post(`${API.ADMIN.RE_ISSUE}`, {});
 
   return data;
 };
