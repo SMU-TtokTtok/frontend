fb5124623c658138e8404b8e3dd9d56f99c3c1d2
chahyunwoo00 <ckgusdn8@naver.com>
2025-07-31 12:45:27 +0900

    feat: 404 not-found 페이지 제작

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import DefaultHeader from '@/common/components/header/defaultHeader';
+import Footer from '@/common/ui/footer';
+import { FOOTER } from '@/common/constants';
+import * as S from '@/components/404/index.css';
+
+export default function NotFound() {
+  return (
+    <>
+      <DefaultHeader />
+      <div className={S.wrapper}>
+        <div className={S.container}>
+          <div className={S.numberText}>404</div>
+          <div className={S.stringTextContainer}>
+            <div className={S.stringText}>문제가 발생했습니다!</div>
+            <div className={S.stringText2}>
+              일시적인 오류입니다. <br className={S.stringText2Break} />
+              잠시 후에 다시 시도해주세요
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer serviceName={FOOTER.serviceName} informationUrl={FOOTER.informationUrl} />
+    </>
+  );
+}
