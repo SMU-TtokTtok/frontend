e051cda3433cac616cdefe42351ce5ba1475ccef
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-01 16:21:32 +0900

    refactor: 사용자부 동아리 소개 suspense 적용

--- src/app/(with-footer)/club/[clubId]/page.tsx
+++ src/app/(with-footer)/club/[clubId]/page.tsx
@@ -1,41 +1,13 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import * as S from '@/components/clubInfo/index.css';
-import BackButton from '@/components/clubInfo/BackButton';
-import ClubProfile from '@/components/clubInfo/ClubProfile';
-import ClubIntroduce from '@/components/clubInfo/ClubIntro';
-import RightSide from '@/components/clubInfo/RightSide';
-import { useParams } from 'next/navigation';
-import { useClubInfo } from '@/hooks/useClubInfo';
+import ClubInfoPage from '@/components/clubInfo';
+import { Suspense } from 'react';
+import LoadingSpinner from '@/common/ui/loading';
 
-export default function Page() {
-  const { clubId } = useParams();
-  const { data } = useClubInfo(Number(clubId));
-  const [isLargeScreen, setIsLargeScreen] = useState(false);
-
-  useEffect(() => {
-    const checkScreenSize = () => {
-      setIsLargeScreen(window.innerWidth >= 1440);
-    };
-
-    checkScreenSize();
-    window.addEventListener('resize', checkScreenSize);
-    return () => window.removeEventListener('resize', checkScreenSize);
-  }, []);
-
+export default function page() {
   return (
-    <div className={S.wrapper}>
-      <div className={S.container}>
-        <div className={S.leftcontainer}>
-          <BackButton />
-          <ClubProfile clubIntro={data} clubId={Number(clubId)} />
-          {!isLargeScreen && <RightSide clubIntro={data} clubId={Number(clubId)} />}
-          <ClubIntroduce introduction={data.content} />
-        </div>
-
-        {isLargeScreen && <RightSide clubIntro={data} clubId={Number(clubId)} />}
-      </div>
-    </div>
+    <Suspense fallback={<LoadingSpinner />}>
+      <ClubInfoPage />
+    </Suspense>
   );
 }
