7ba0fd0df6f5fdf4ba84cc5070c73ed9bff37edf
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-03 23:19:53 +0900

    fix: 수정 아무것도 안할때 예외처리

--- src/components/admin/clubInfo/api/pactchClubInfo.ts
+++ src/components/admin/clubInfo/api/pactchClubInfo.ts
@@ -22,14 +22,22 @@ export interface PatchClubInfoBody {
 export const patchClubInfo = async (body: PatchClubInfoBody, clubId: string) => {
   console.log(body);
   const formData = new FormData();
+  let hasData = false;
+
   if (body.request && Object.keys(body.request).length > 0) {
     formData.append(
       'request',
       new Blob([JSON.stringify(body.request)], { type: 'application/json' }),
     );
+    hasData = true;
   }
   if (body.profileImageUrl) {
     formData.append('profileImage', body.profileImageUrl);
+    hasData = true;
+  }
+
+  if (!hasData) {
+    return;
   }
 
   const data = await adminClient.patch(`/clubs/${clubId}/content`, formData);
