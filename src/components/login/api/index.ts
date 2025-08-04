32e1488e9eaf0951573812b0bcf6c31cc2ed9fb4
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-04 23:40:51 +0900

    feat: 로그아웃 api 함수 생성

--- src/components/login/api/index.ts
+++ src/components/login/api/index.ts
@@ -6,3 +6,13 @@ export const postLogin = async (body: { email: string; password: string }) => {
   localStorage.setItem('email', data.data.user.email);
   return data;
 };
+
+export const postLogout = async (email: string) => {
+  const data = await mainClient.post(`/api/user/auth/logout?useremail=${email}`, null);
+  localStorage.removeItem('name');
+  localStorage.removeItem('email');
+
+  window.location.href = '/login';
+
+  return data;
+};
