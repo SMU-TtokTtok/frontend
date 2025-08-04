e50634a65ad4fc6e9da695909338ab55d67d0559
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-04 22:54:51 +0900

    feat: 로그인시 데이터 저장

--- src/components/login/api/index.ts
+++ src/components/login/api/index.ts
@@ -2,6 +2,7 @@ import { mainClient } from '@/common/apis/ttockTtockClient';
 
 export const postLogin = async (body: { email: string; password: string }) => {
   const data = await mainClient.post('/api/user/auth/login', body);
-
+  localStorage.setItem('name', data.data.user.name);
+  localStorage.setItem('email', data.data.user.email);
   return data;
 };
