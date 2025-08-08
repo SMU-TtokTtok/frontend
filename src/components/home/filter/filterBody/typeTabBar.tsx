8949ef44b01b9a60c8627484a233c6ee9860b77d
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-08-08 23:09:40 +0900

    refactor: preservequery 추가 및 버튼클릭시  clubUniv 키 삭제

--- src/components/home/filter/filterBody/typeTabBar.tsx
+++ src/components/home/filter/filterBody/typeTabBar.tsx
@@ -3,24 +3,37 @@ import { FILTER_CONFIG } from '@/common/constants';
 import Button from '@/common/ui/button';
 import { FilterProps } from './filterBodyBottom';
 import * as S from '../filter.css';
+import { useSearchParams } from 'next/navigation';
 
 function TypeTabBar({ selectedOptions }: FilterProps) {
+  const searchParams = useSearchParams();
+
+  const existingQuery = Object.fromEntries(searchParams.entries());
+  const { clubUniv, ...restQuery } = existingQuery;
+  console.log('club Univ:', clubUniv);
+
   return (
     <div className={S.firstTypeWrapper}>
-      {FILTER_CONFIG.type.map((option) => (
-        <QueryLink key={option.value} extraQuery={{ type: option.value }}>
-          <Button
-            key={option.value}
-            variant="none"
-            className={S.ButtonStyle({
-              style: 'body',
-              isSelected: selectedOptions?.type === option.value,
-            })}
-          >
-            {option.label}
-          </Button>
-        </QueryLink>
-      ))}
+      {FILTER_CONFIG.type.map((option) => {
+        const finalQuery = {
+          ...restQuery,
+          type: option.value,
+        };
+        return (
+          <QueryLink key={option.value} preserveQuery={false} extraQuery={finalQuery}>
+            <Button
+              key={option.value}
+              variant="none"
+              className={S.ButtonStyle({
+                style: 'body',
+                isSelected: selectedOptions?.type === option.value,
+              })}
+            >
+              {option.label}
+            </Button>
+          </QueryLink>
+        );
+      })}
     </div>
   );
 }
