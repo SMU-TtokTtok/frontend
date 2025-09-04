0715ce203af2df7b0a944eb8db0fce81e6d05bfa
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-09-05 04:16:40 +0900

    feat: 사용자 피드백 url 추가

--- src/common/ui/footer/index.tsx
+++ src/common/ui/footer/index.tsx
@@ -7,20 +7,28 @@ import { ROUTES } from '@/common/constants/routes';
 interface FooterProps {
   serviceName: string;
   informationUrl: string;
+  feedbackUrl: string;
 }
 
-function Footer({ serviceName, informationUrl }: FooterProps) {
+function Footer({ serviceName, informationUrl, feedbackUrl }: FooterProps) {
   return (
     <div className={S.container}>
       <div className={S.innerWrapper}>
         <Link href={ROUTES.ADMIN_LOGIN}>
           <Image src={Mainlogo} className={S.logo} alt="메인로고" />
         </Link>
-        <p className={S.content}>
-          <Link href={informationUrl} target="_blank" rel="noopener noreferrer">
-            개인정보 처리방침
-          </Link>
-        </p>
+        <div className={S.rowsort}>
+          <p className={S.content}>
+            <Link href={informationUrl} target="_blank" rel="noopener noreferrer">
+              개인정보 처리방침
+            </Link>
+          </p>
+          <p className={S.content}>
+            <a href={feedbackUrl} target="_blank" rel="noopener noreferrer">
+              피드백 하러가기
+            </a>
+          </p>
+        </div>
         <p className={S.content}>Copyright ⓒ {serviceName}. All Rights Reserved</p>
       </div>
     </div>
