56611714881af5190c5de6d6113c425ba84eb552
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-11-26 19:15:28 +0900

    refactor: 반응형처리 모바일 버전 컴포넌트 추상화

---
+++ src/common/components/header/adminHeader/mobile.tsx
@@ -0,0 +1,47 @@
+import { ROUTES } from '@/common/constants/routes';
+import Header from '@/common/ui/header';
+import mainlogo from '@/assets/mainlogo_wh.svg';
+import Image from 'next/image';
+import Link from 'next/link';
+import * as S from './adminHeader.css';
+import hamburger from '@/assets/hambuger.svg';
+import { AdminProfile } from '@/common/model/adminProfile';
+import { useState } from 'react';
+
+import { useOutsideClick } from '@/hooks/useOutsideClick';
+import { usePreventScroll } from '@/hooks/usepreventScroll';
+import MobiledSideMenu from './mobildSideMenu';
+
+interface MobileProps {
+  isVisible: boolean;
+  handleLogoutClick: () => void;
+  profile: AdminProfile | null;
+}
+
+function Mobile({ isVisible, handleLogoutClick, profile }: MobileProps) {
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+  const handleToggleSideMenu = () => {
+    setIsSideMenuOpen(!isSideMenuOpen);
+  };
+  const ref = useOutsideClick<HTMLDivElement>(() => setIsSideMenuOpen(false));
+  usePreventScroll(isSideMenuOpen);
+
+  return (
+    <Header isVisible={isVisible} className={S.MobileInnerWrapper}>
+      <Link href={ROUTES.ADMIN}>
+        <Image src={mainlogo} className={S.Logo} alt="Main Logo" />
+      </Link>
+      <Image
+        src={hamburger}
+        className={S.MobileHamburgerIcon}
+        alt="Menu"
+        onClick={handleToggleSideMenu}
+      />
+      {isSideMenuOpen && (
+        <MobiledSideMenu handleLogoutClick={handleLogoutClick} mobileRef={ref} profile={profile} />
+      )}
+    </Header>
+  );
+}
+
+export default Mobile;
