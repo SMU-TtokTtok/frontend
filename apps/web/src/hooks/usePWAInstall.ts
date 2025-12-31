74d7c3142342940f8541640cff81cba23b149954
chahyunwoo00 <ckgusdn8@naver.com>
2025-12-31 23:27:39 +0900

    fix: 린트오류 해결

--- apps/web/src/hooks/usePWAInstall.ts
+++ apps/web/src/hooks/usePWAInstall.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from 'react';
 
 interface BeforeInstallPromptEvent extends Event {
