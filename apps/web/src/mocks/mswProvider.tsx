1630b46c6d9ce38d8577e11b38ec22b765602609
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2026-07-28 22:11:19 +0900

    refactor: mswProvider 로직 변경

--- apps/web/src/mocks/mswProvider.tsx
+++ apps/web/src/mocks/mswProvider.tsx
@@ -2,28 +2,27 @@
 import { useEffect, useRef, useState } from 'react';
 
 export const MSWProvider = ({ children }: { children: React.ReactNode }) => {
-  const isMockingEnabled = process.env.NEXT_PUBLIC_API_MOCKING === 'enabled';
-  const [isMockingReady, setIsMockingReady] = useState(!isMockingEnabled);
+  const [isMocking, setIsMocking] = useState(false);
   const isWorkerStarted = useRef(false);
 
   useEffect(() => {
     async function enableApiMocking() {
-      if (typeof window !== 'undefined' && !isWorkerStarted.current && isMockingEnabled) {
+      if (
+        typeof window !== 'undefined' &&
+        !isWorkerStarted.current &&
+        process.env.NEXT_PUBLIC_API_MOCKING === 'enabled'
+      ) {
         isWorkerStarted.current = true;
         const { worker } = await import('./browser');
         await worker.start();
-        setIsMockingReady(true);
+        setIsMocking(true);
       }
     }
 
     enableApiMocking();
-  }, [isMockingEnabled]);
+  }, []);
 
-  if (!isMockingEnabled) {
-    return <>{children}</>;
-  }
-
-  if (!isMockingReady) {
+  if (!isMocking) {
     return null;
   }
 
